c56a036da1d3409ebecd7049834e371d4d3f6e8f
ezteddy <teddyezz05@gmail.com>
2026-02-01 11:50:41 +0800

    修改网站标题

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: '来屋里家具',
+  title: '来屋里 | 智能升降桌',
+  description: '佛山工厂直发，专注高品质电动升降桌，为健康办公而生',
+  locale: 'cn',
 };
